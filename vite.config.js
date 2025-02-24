0154b54399a832483a80055e7a13b799f64d1e0e
juanjose <pasantejjsoluciones@gmail.com>
2025-02-24 17:07:12 -0500

    Descripción de los cambios

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto ayuda a que se resuelvan las rutas correctamente
+  base: './', 
   build: {
     outDir: 'dist',
   }
